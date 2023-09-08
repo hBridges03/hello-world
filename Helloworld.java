d0c92029fd5155c0feba279f6f1021203dc78a9a
Hayden Bridges <hbridge4@email.cpcc.edu>
2023-09-08 10:27:34 -0400

    Revert "Update HelloWorld.java"
    This reverts commit fda76ed7115062e679d1b9ab7f62e54ad7e6bd8e.

--- Helloworld.java
+++ Helloworld.java
@@ -3,7 +3,6 @@ public class Helloworld
    public static void main(String[] args)
    {
       System.out.println("Hello World");
-      System.out.println("Program modified, but able to revert to original version using GitHub Desktop.");
    }
 
 }
